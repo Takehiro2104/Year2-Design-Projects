8404cc35ebc1f483907c5e7095879bd3bf443ed1
Takehiro2104 <150436410+Takehiro2104@users.noreply.github.com>
2023-11-10 19:04:49 +0800

    Add files via upload

---
+++ Distance_LEDs.ino
@@ -0,0 +1,67 @@
+/* Example code for HC-SR04 ultrasonic distance sensor with Arduino. No library required. More info: https://www.makerguides.com */
+
+// Define Trig and Echo pin:
+#define trigPin 2
+#define echoPin 3
+#define led1pin 4 //Red
+#define led2pin 5 //Yellow
+#define led3pin 6 //Green
+
+
+// Define variables:
+long duration;
+int distance;
+
+
+void setup() {
+  // Define inputs and outputs:
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led1pin, OUTPUT);
+  pinMode(led2pin, OUTPUT);
+  pinMode(led3pin, OUTPUT);
+  //Begin Serial communication at a baudrate of 9600:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Clear the trigPin by setting it LOW:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+
+  // Trigger the sensor by setting the trigPin high for 10 microseconds:
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Read the echoPin, pulseIn() returns the duration (length of the pulse) in microseconds:
+  duration = pulseIn(echoPin, HIGH);
+  // Calculate the distance:
+  distance = duration * 0.034 / 2;
+
+
+  // Print the distance on the Serial Monitor (Ctrl+Shift+M):
+  Serial.print("Distance = ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  // LED light up  due to distance 
+  // Run time same as function needs to called every loop.
+  if (distance < 15){
+    digitalWrite(led1pin, HIGH); 
+    digitalWrite(led2pin, LOW);
+    digitalWrite(led3pin, LOW);
+    
+  }
+  else if (distance < 30){
+    digitalWrite(led2pin, HIGH);
+    digitalWrite(led3pin, LOW);
+    digitalWrite(led1pin, LOW);
+  }
+  else{
+    digitalWrite(led3pin, HIGH);
+    digitalWrite(led1pin, LOW);
+    digitalWrite(led2pin, LOW);
+  }
+
+}
