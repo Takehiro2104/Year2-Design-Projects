b27ae0886957a92d9224f4dbc27ff9c3af1c376a
cix07 <150444358+cix07@users.noreply.github.com>
2023-11-13 17:14:57 +0800

    Update RedundantBodyLED.ino

--- RedundantBodyLED.ino
+++ RedundantBodyLED.ino
@@ -3,7 +3,7 @@ int LDRPin2;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(8, OUTPUT);
+  pinMode(A4, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -12,12 +12,12 @@ void loop() {
   LDRPin1 = analogRead(A0);
   LDRPin2 = analogRead(A1);
 
-  if(LDRPin1 <60 && LDRPin2 <60){
-    digitalWrite(8, HIGH);
+  if(LDRPin1 < 80 && LDRPin2 < 80){
+    analogWrite(A4, 255);
   }else{
-    digitalWrite(8,LOW);
+    analogWrite(A4,0);
   }
   Serial.println(LDRPin1);
   delay(100);
-
 }
+
